1f7964e4646d31ff02373a414bc0b589741ffc8f
gwvsol <gwvsol@gmail.com>
2019-02-04 12:37:54 +0200

    modified the startup file of the application

--- startapp.sh
+++ startapp.sh
@@ -1,4 +1,4 @@
 DIR="/app/wsgi"
 cd $DIR
 source venv/bin/activate
-screen python3.6 api/app.py
+screen uwsgi --http-socket 0.0.0.0:5000 --manage-script-name --protocol=http --plugin pyththon3 -w app:app
